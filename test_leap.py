ba5eb3a72af40e6e2f6cf81d8464cd0bc52444c0
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2026-01-16 10:00:38 +0530

    Update test_leap.py

--- test_leap.py
+++ test_leap.py
@@ -1,8 +1,10 @@
 from leap import is_leap
 
 def test_is_leap():
-    assert is_leap(2020) == True 
-def test_not_leap():    
-    assert is_leap(1900) == False   
+    assert is_leap(2020) is True
+
+def test_not_leap():
+    assert is_leap(1900) is False
+
 def test_2000():
-    assert is_leap(2000) == True     
+    assert is_leap(2000) is True
