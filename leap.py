4356c5c107d8ebc37da14c71550e650a7582933a
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2026-01-16 10:03:06 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,10 +1,12 @@
+import sys
+
 def is_leap(year):
     return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
 
-
 if __name__ == "__main__":
-    y = int(input("Enter a year: "))
-    if is_leap(y):
-        print(f"{y} is a leap year.")
-    else:
-        print(f"{y} is not a leap year.")
+    if len(sys.argv) == 2:
+        y = int(sys.argv[1])
+        if is_leap(y):
+            print(f"{y} is leap year.")
+        else:
+            print(f"{y} is not leap year.")
